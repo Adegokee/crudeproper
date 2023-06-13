79763bf53b2c679c834f95c10a32d801515d8cd5
adegokee <ababatunde022@gmail.com>
2023-06-13 13:22:18 +0100

    apireactcrude

--- src/ReviewContext.jsx
+++ src/ReviewContext.jsx
@@ -1,5 +1,6 @@
 import React, {createContext, useState, useEffect} from 'react'
-const url = 'https://jsonplaceholder.typicode.com/posts'
+// const url = 'https://jsonplaceholder.typicode.com/posts'
+const url = "http://localhost:8000/api/v1/alltodo/"
 export const ReviewContext= createContext()
 const GetContext = ({children}) => {
     const [tosin, setTosin]=useState([])
@@ -15,7 +16,9 @@ const GetContext = ({children}) => {
 
 
    const addReview = async (newReview) => {
-    const url = 'https://jsonplaceholder.typicode.com/posts'
+    // const url = 'https://jsonplaceholder.typicode.com/posts'
+    const url = "http://localhost:8000/api/v1/createtodo/"
+
     const response = await fetch(url,{
       method:'POST',
       headers: {'Content-Type': 'application/json'},
@@ -29,7 +32,7 @@ const GetContext = ({children}) => {
 
    const deleteReview = async(id) => {
     if(window.confirm('Are you sure you want to delete this?')){
-      await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+      await fetch(`http://localhost:8000/api/v1/delete/${id}`,{
           method: 'DELETE',
       })
       setTosin(tosin.filter((item)=> item.id !== id))
